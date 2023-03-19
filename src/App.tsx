f869779d9839f07eb21f5b8968b935278952ac1c
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2023-03-19 15:31:03 +0300

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import update from 'immutability-helper'
 import cn from "classnames";
 
--- src/Redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import stateReducer from "./reducers/stateReducer";
-
-
-// const rootReducer = combineReducers({
-//     stateReducer,
-// })
-
-// export const store = configureStore(rootReducer)
